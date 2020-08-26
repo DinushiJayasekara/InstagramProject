f42d828ef4b805894ef3e88fd43fca44aab73931
DinushiJayasekara <jkdrj02@gmail.com>
2020-08-26 13:44:21 +0530

    first commit

---
+++ Bookworms/resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,10 @@
+@component('mail::message')
+# Welcome to Bookworms
+
+This is a community of fellow bookworms and we love that you joined us!
+Have fun checking out the best novels to read! 🐛
+
+
+Cheers,<br>
+❤ Dinu
+@endcomponent
